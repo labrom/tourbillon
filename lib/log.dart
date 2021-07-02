9d243824d1e0c01a815a89c14fa1d220f43f302a
Romain Laboisse <labrom@gmail.com>
2021-07-02 16:06:12 +0200

    Better logging configuration

--- lib/log.dart
+++ lib/log.dart
@@ -1,4 +1,35 @@
-import 'package:flutter/foundation.dart';
 import 'package:logger/logger.dart';
 
-final log = Logger(level: kDebugMode ? Level.verbose : Level.warning);
+final log = Logger(
+  level: Level
+      .verbose, // level is used in debug mode only, nothing's logged in release mode
+  printer: _Printer(),
+);
+
+class _Printer extends LogPrinter {
+  @override
+  List<String> log(LogEvent event) =>
+      event.level == Level.error || event.level == Level.debug
+          ? _logError(event)
+          : ['${_levelPrefix[event.level]}: ${event.message}'];
+
+  List<String> _logError(LogEvent event) {
+    var lines = <String>[event.message];
+    if (event.error != null) {
+      lines.add(event.error.toString());
+    }
+    if (event.stackTrace != null) {
+      lines.add(event.stackTrace.toString());
+    }
+    return lines;
+  }
+}
+
+const _levelPrefix = {
+  Level.debug: 'D',
+  Level.error: 'E',
+  Level.info: 'I',
+  Level.verbose: 'V',
+  Level.warning: 'W',
+  Level.wtf: 'WTF',
+};
