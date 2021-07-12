9614b15bce88b0fa847ca605b0e904b878ac9803
Romain Laboisse <labrom@gmail.com>
2021-07-12 06:36:35 -0700

    Add ChangeNotifierScope

---
+++ lib/change_notifier_scope.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+
+/// A widget that gives all widgets under itself access to a [ChangeNotifier].
+///
+/// It is OK to use this widget anywhere in the widget tree, as it will create
+/// a [ChangeNotifierProvider] only if it cannot find one in its parent tree.
+/// If an instance of this widget is used in the parent tree, no new [ChangeNotifierProvider]
+/// is created.
+class ChangeNotifierScope<CN extends ChangeNotifier> extends StatelessWidget {
+  final Widget? child;
+  final CN Function(BuildContext context) create;
+  final ChangeNotifierBuilder<CN> builder;
+
+  ChangeNotifierScope(
+    this.create, {
+    required this.builder,
+    this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    try {
+      var changeNotifier = Provider.of<CN>(context, listen: false);
+      return builder(context, changeNotifier, child);
+    } on ProviderNotFoundException {
+      return ChangeNotifierProvider<CN>(
+        create: create,
+        child: Consumer<CN>(
+          builder: (context, changeNotifier, child) =>
+              builder(context, changeNotifier, child),
+          child: child,
+        ),
+      );
+    }
+  }
+}
+
+typedef ChangeNotifierBuilder<CN extends ChangeNotifier> = Function(
+    BuildContext context, CN notifier, Widget? child);
