b7cb9257b08478fbff01391d7a83b0237f2099cc
Romain Laboisse <labrom@gmail.com>
2021-07-13 19:09:23 -0700

    Load data when creating FirestoreView

--- lib/data_provider.dart
+++ lib/data_provider.dart
@@ -5,11 +5,15 @@ import 'firestore.dart';
 
 class DataProvider extends ChangeNotifier {
   final String path;
+  bool _loaded = false;
+
   List<DocumentSnapshot> data = [];
 
   DataProvider(this.path);
 
   void load() async {
+    if (_loaded) return;
+    _loaded = true;
     FirebaseFirestore.instance.collection(path).snapshots().listenUnique(
         (snapshots) {
       data = snapshots.docs;
--- lib/firestore_view.dart
+++ lib/firestore_view.dart
@@ -16,11 +16,14 @@ class FirestoreView extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierScope<DataProvider>(
       (_) => DataProvider(path),
-      builder: (context, provider, _) => ListView.builder(
-        itemCount: provider.data.length,
-        itemBuilder: (context, index) =>
-            Text(provider.data[index].getOrDefault('name', '')),
-      ),
+      builder: (context, provider, _) {
+        provider.load();
+        return ListView.builder(
+          itemCount: provider.data.length,
+          itemBuilder: (context, index) =>
+              Text(provider.data[index].getOrDefault('name', '')),
+        );
+      },
     );
   }
 }
