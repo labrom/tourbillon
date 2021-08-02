68eb39515abbf47320f53ecdc65c20102b8cfaaa
Romain Laboisse <labrom@gmail.com>
2021-08-01 19:16:06 -0700

    User management view model

--- test/fake_firestore.dart
+++ test/fake_firestore.dart
@@ -8,3 +8,10 @@ class FakeFirestoreWrapper implements FirestoreInterface {
   @override
   FirebaseFirestore get instance => fake;
 }
+
+extension FakeFirebaseFirestoreExtension on FakeFirebaseFirestore {
+  /// FakeFirebaseFirestore doesn't seem to notify on the last write operation,
+  /// so adding this function that performs an extra write to make sure all
+  /// relevant writes are notified on.
+  Future<void> flush() async => await collection('_').add({});
+}
