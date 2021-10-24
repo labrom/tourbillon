2ea9588463b373ea9257e25f519e12552898b503
Romain Laboisse <labrom@gmail.com>
2021-10-24 10:29:27 -0700

    Package imports cleanup

--- lib/firestore_view.dart
+++ lib/firestore_view.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 import 'change_notifier_scope.dart';
 import 'data_provider.dart';
